62c71c4d9a80eefb21565fb7a83c61ffd0da2f92
20120312 <letankietpdcd@gmail.com>
2022-12-18 15:31:21 +0700

    init

---
+++ MediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        class MediaFile
+        {
+            public string Name { get; set; } 
+            public string Author { get; set; }
+            public string Thumbnail { get; set; }
+        }
+
+        List<MediaFile> mediaFiles = new List<MediaFile>(); 
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mediaFiles.Add(new MediaFile() { 
+                Name = "Waiting for you",
+                Author = "Mono, Onionn",
+                Thumbnail= "/Images/waiting_for_you.png"
+            });
+            mediaFiles.Add(new MediaFile()
+            {
+                Name = "Tai vi sao",
+                Author = "MCK",
+                Thumbnail = "/Images/waiting_for_you.png"
+            });
+            mediaFiles.Add(new MediaFile()
+            {
+                Name = "Waiting for you",
+                Author = "Mono, Onionn",
+                Thumbnail = "/Images/waiting_for_you.png"
+            });
+            mediaFiles.Add(new MediaFile()
+            {
+                Name = "Tai vi sao",
+                Author = "MCK",
+                Thumbnail = "/Images/waiting_for_you.png"
+            });
+
+            playlistListView.ItemsSource = mediaFiles;
+        }
+    }
+}
